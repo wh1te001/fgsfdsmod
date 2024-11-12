38f59b03a71ac85ef19219209c9085ebba23a0f1
wh1te001 <wh1te.ggle@gmail.com>
2024-11-13 01:27:08 +0300

    basic changes for build, main class, toml

--- src/main/java/net/wh1te001/fgsfdsmod/FgsfdsMod.java
+++ src/main/java/net/wh1te001/fgsfdsmod/FgsfdsMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package net.wh1te001.fgsfdsmod;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,13 +19,15 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("fgsfdsmod")
+public class FgsfdsMod
 {
+    public static final String MOD_ID = "fgsfdsmod";
+
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public FgsfdsMod() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
@@ -48,7 +50,6 @@ public class ExampleMod
 
     private void doClientStuff(final FMLClientSetupEvent event) {
         // do something that can only be done on the client
-        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
     }
 
     private void enqueueIMC(final InterModEnqueueEvent event)
@@ -59,10 +60,7 @@ public class ExampleMod
 
     private void processIMC(final InterModProcessEvent event)
     {
-        // some example code to receive and process InterModComms from other mods
-        LOGGER.info("Got IMC {}", event.getIMCStream().
-                map(m->m.getMessageSupplier().get()).
-                collect(Collectors.toList()));
+
     }
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
